20471aa0e6dc847828c8fb3eaeb514691cf8909a
kosebaris1 <kosebaris464@gmail.com>
2025-08-30 23:12:34 +0300

    Database connection

--- Accounting.API/Program.cs
+++ Accounting.API/Program.cs
@@ -1,4 +1,8 @@
 
+using Accounting.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 namespace Accounting.API
 {
     public class Program
@@ -7,16 +11,19 @@ namespace Accounting.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
