ee197f711f187dd6fff3158560b802649641369d
kosebaris1 <kosebaris464@gmail.com>
2025-09-03 21:57:51 +0300

    feat: completed GenericInterface and GenericRepository classes

---
+++ Accounting.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        int Count();
+        void UpdateAsync(T entity);
+        void ChangeStatusAsync(T entity);
+
+        Task AddAsync(T entity);
+
+
+
+
+    }
+}
---
+++ Accounting.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using Accounting.Core.Models;
+using Accounting.Core.Repositories;
+using Accounting.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+
+        }
+
+        public void ChangeStatusAsync(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public int Count()
+        {
+           return _dbSet.Count();
+        }
+
+        public  IQueryable<T> GetAllAsync()
+        {
+            return  _dbSet.Where(x=> x.Status == true).AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void UpdateAsync(T entity)
+        {
+           _dbSet.Update(entity);
+        }
+
+        public  IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
