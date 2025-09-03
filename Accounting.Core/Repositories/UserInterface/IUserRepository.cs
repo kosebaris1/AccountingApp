75de01c3cf4c895a4c69990f188883d46736c861
kosebaris1 <kosebaris464@gmail.com>
2025-09-03 22:13:04 +0300

    feat(models): define model interfaces

---
+++ Accounting.Core/Repositories/UserInterface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Accounting.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Core.Repositories.UserInterface
+{
+    public interface IUserRepository: IGenericRepository<User>
+    {
+    }
+}
